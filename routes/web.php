87323a2b47721324409cf589ba2abe771bb1451f
mayitoae84 <mario.enriquez@gigapixel.com.br>
2021-11-30 13:34:11 -0300

    Preparando os Middleware!

--- routes/web.php
+++ routes/web.php
@@ -16,48 +16,48 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'site\xtrafer\HomeController@index')->name('home')->middleware(['injectFlgPage:home']);
 
-Route::get('about_us', 'Site\minuzzi\aboutController@index')->name('about_us')->middleware(['injectFlgPage:about_us']);
+Route::get('about_us', 'site\xtrafer\aboutController@index')->name('about_us')->middleware(['injectFlgPage:about_us']);
 
-Route::get('about_us_details', 'Site\minuzzi\aboutController@index')->name('about_us_details')->middleware(['injectFlgPage:about_us_details']);
+Route::get('about_us_details', 'site\xtrafer\aboutController@index')->name('about_us_details')->middleware(['injectFlgPage:about_us_details']);
 
-Route::get('budget', 'Site\minuzzi\BudgetController@index')->name('budget')->middleware(['injectFlgPage:budget']);
+Route::get('budget', 'site\xtrafer\BudgetController@index')->name('budget')->middleware(['injectFlgPage:budget']);
 
-Route::get('budget/{id}', 'Site\minuzzi\BudgetController@index')->name('budget')->middleware(['injectFlgPage:budget']);
+Route::get('budget/{id}', 'site\xtrafer\BudgetController@index')->name('budget')->middleware(['injectFlgPage:budget']);
 
-Route::get('blog', 'Site\minuzzi\BlogController@index')->name('blog')->middleware(['injectFlgPage:blog']);
+Route::get('blog', 'site\xtrafer\BlogController@index')->name('blog')->middleware(['injectFlgPage:blog']);
 
-Route::get('blog_details/{id}', 'Site\minuzzi\BlogController@getPost')->middleware(['injectFlgPage:blog_details']);
+Route::get('blog_details/{id}', 'site\xtrafer\BlogController@getPost')->middleware(['injectFlgPage:blog_details']);
 
-Route::get('product_details/{id}', 'Site\minuzzi\ProductController@getPost')->middleware(['injectFlgPage:product_details']);
+Route::get('product_details/{id}', 'site\xtrafer\ProductController@getPost')->middleware(['injectFlgPage:product_details']);
 
-Route::get('products', 'Site\minuzzi\ProductController@index')->name('products')->middleware(['injectFlgPage:products']);
+Route::get('products', 'site\xtrafer\ProductController@index')->name('products')->middleware(['injectFlgPage:products']);
 
 
-Route::get('contact', 'Site\minuzzi\ContactController@index')->name('contact')->middleware(['injectFlgPage:contact']);
+Route::get('contact', 'site\xtrafer\ContactController@index')->name('contact')->middleware(['injectFlgPage:contact']);
 
-Route::post('contact', 'Site\minuzzi\ContactController@store');
+Route::post('contact', 'site\xtrafer\ContactController@store');
 
-Route::get('search', 'Site\minuzzi\SearchController@search')->middleware(['injectFlgPage:search']);
+Route::get('search', 'site\xtrafer\SearchController@search')->middleware(['injectFlgPage:search']);
 
-Route::get('faq', 'Site\minuzzi\FaqController@index')->middleware(['injectFlgPage:faq']);
+Route::get('faq', 'site\xtrafer\FaqController@index')->middleware(['injectFlgPage:faq']);
 
-Route::get('support', 'Site\minuzzi\SupportController@index')->middleware(['injectFlgPage:support']);
+Route::get('support', 'site\xtrafer\SupportController@index')->middleware(['injectFlgPage:support']);
 
-Route::get('manual', 'Site\minuzzi\ManualController@index')->middleware(['injectFlgPage:manual']);
+Route::get('manual', 'site\xtrafer\ManualController@index')->middleware(['injectFlgPage:manual']);
 
-Route::get('term', 'Site\minuzzi\TermController@index')->name('term')->middleware(['injectFlgPage:term']);
+Route::get('term', 'site\xtrafer\TermController@index')->name('term')->middleware(['injectFlgPage:term']);
 
-Route::get('privacy', 'Site\minuzzi\PrivacyController@index')->name('privacy')->middleware(['injectFlgPage:privacy']);
+Route::get('privacy', 'site\xtrafer\PrivacyController@index')->name('privacy')->middleware(['injectFlgPage:privacy']);
 
-Route::get('certified', 'Site\minuzzi\PrivacyController@certified')->name('certified')->middleware(['injectFlgPage:certified']);
+Route::get('certified', 'site\xtrafer\PrivacyController@certified')->name('certified')->middleware(['injectFlgPage:certified']);
 
-Route::get('register', 'Site\minuzzi\RegisterController@index')->name('register')->middleware(['injectFlgPage:register']);
+Route::get('register', 'site\xtrafer\RegisterController@index')->name('register')->middleware(['injectFlgPage:register']);
 
-Route::post('register', 'Site\minuzzi\RegisterController@store');
+Route::post('register', 'site\xtrafer\RegisterController@store');
 
-Route::get('lang/{lang}', 'Site\minuzzi\LanguageController@switchLang')->name('lang.switch');
+Route::get('lang/{lang}', 'site\xtrafer\LanguageController@switchLang')->name('lang.switch');
 
-Route::get('/bilingualism', 'Site\minuzzi\\BilingualismController@index');
+Route::get('/bilingualism', 'site\xtrafer\\BilingualismController@index');
 
 // Route::get('/test', function () {
 // 	App::setLocale('');
